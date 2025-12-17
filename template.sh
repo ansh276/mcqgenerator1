c44c106d1c264d01ee8ff6d5ecb58344cc424e47
ansh276 <aaransh27@gmail.com>
2025-12-17 16:58:45 +0530

    folder structure updated

---
+++ template.sh
@@ -0,0 +1,18 @@
+# Creating directory 
+mkdir -p src
+mkdir -p src/mcqgenerator
+mkdir -p experiment 
+
+# Creating files
+touch src/__init__.py
+touch src/mcqgenerator/__init__.py
+touch src/mcqgenerator/logger.py
+touch src/mcqgenerator/mcqgenerator.py
+touch src/mcqgenerator/utils.py
+touch .env 
+touch setup.py
+touch experiment/mcqgen.ipynb 
+touch requirements.txt
+
+
+echo "Directory and files created successfully!."
